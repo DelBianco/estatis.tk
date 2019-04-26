da1988949a1376bb3af7b388fe97c64f57769d5e
André Giuffrida <andre.giuffrida@runweb.com.br>
2019-04-26 13:57:43 -0300

    Teste de deploy

---
+++ Estatistk.js
@@ -0,0 +1,20 @@
+// Constructor
+function Estatistk() {
+
+}
+// class methods
+Estatistk.prototype.endpoints = function() {
+    return ['set1','set2'];
+};
+
+Estatistk.prototype.data = function(endpoint){
+    if(endpoint === 'set1'){
+        return [1,2,3,4,5,6,7,8,9,0];
+    }
+    if(endpoint === 'set2'){
+        return [0,9,8,7,6,5,4,3,2,1];
+    }
+};
+
+// export the class
+module.exports = Estatistk;
--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-
+// Now, import
+var Estatistk = require('./Estatistk');
 var express = require("express");
 var app = express();
 var router = express.Router();
 
 var path = __dirname + '/views/';
-const PORT = 8080;
+
+const PORT = 8088;
 const HOST = '0.0.0.0';
 
-router.use(function (req,res,next) {
+router.use(function (req, res, next) {
     console.log("/" + req.method);
     next();
 });
 
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
     res.sendFile(path + "index.html");
 });
 
-router.get("/api",function(req,res){
-    res.json({ message: 'API LIVES' });
+
+var est = new Estatistk();
+
+router.get("/api", function (req, res) {
+    res.json({endpoint: est.endpoints()});
+});
+
+est.endpoints().forEach(function (endpoint) {
+    console.log(endpoint);
+    router.get("/api/"+endpoint, function (req, res) {
+        res.json({data: est.data(endpoint)});
+    });
 });
 
 app.use(express.static(path));
 app.use("/", router);
 
-app.listen(8080, function () {
-    console.log('Estatis.tk running on port 8080!')
+app.listen(8088, function () {
+    console.log('Estatis.tk running on port 8088!')
 })
